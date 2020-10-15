a1366effaabd9a82ae1e33df56974e4026529758
Nicolle Romero <nicollearomero@gmail.com>
2020-10-15 08:51:35 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { QueryBuilderComponent } from '@syncfusion/ej2-react-querybuilder';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <div>
+//       <QueryBuilderComponent />
+//     </div>
+//   );
+// }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (<div><QueryBuilderComponent /></div>);
+  }
+}
