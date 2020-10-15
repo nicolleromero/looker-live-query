4ed67ffcf301e860af319e971a824b302f8034fb
Stephanie Resis <sresis@umich.edu>
2020-10-15 16:40:08 -0700

    add formatter

--- src/formatter.js
+++ src/formatter.js
@@ -1,7 +1,18 @@
 import React, { useRef, useState } from 'react';
-import { Button, Form, FormControl } from 'react-bootstrap';
+import sqlFormatter from 'sql-formatter';
 
-export default function ProjectForm(props) {
+export default function Formatter(props) {
+    const [text, setText] = React.useState([]);
+    // update it
+    const sqlText = "SELECT * FROM table1 WHERE customers > 100;";
+    const formattedText = sqlFormatter.format(sqlText);
+    console.log(formattedText);
 
+   
+    
+
+    return(
+        <div>{formattedText}</div>
+    )
 
 }
