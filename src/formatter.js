61fcbba7960de4d67f6d2e76a1a57131fb3dd06e
Stephanie Resis <sresis@umich.edu>
2020-10-15 17:30:10 -0700

    add prism files

--- src/formatter.js
+++ src/formatter.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import sqlFormatter from 'sql-formatter';
+import Prism from "prismjs";
+
 
 export default function Formatter(props) {
-    const [text, setText] = useState('');
 
-    // update it
-    const sqlText = "SELECT * FROM table1 WHERE customers > 100;";
     const formattedText = sqlFormatter.format(props.query);
-    console.log(props.query);
-
 
     return (
         <div className="center-children">
