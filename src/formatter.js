a8e0d2a1bf8ea699229564937bb9976a08d9425d
Nicolle Romero <nicollearomero@gmail.com>
2020-10-15 18:27:12 -0700

    Added SQL text colors

--- src/formatter.js
+++ src/formatter.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import sqlFormatter from 'sql-formatter';
 import Prism from "prismjs";
+import 'prismjs/components/prism-sql';
+
 import "./prism.css";
 
-
 export default function Formatter(props) {
-    Prism.highlightAll();
+  const formattedText = sqlFormatter.format(props.query);
+  const highlighted = Prism.highlight(formattedText, Prism.languages.sql, 'sql');
 
-    const formattedText = sqlFormatter.format(props.query);
-
-    return (
-        <div className="center-children">
-            <pre className="format-sql"><code className="language-SQL">{formattedText}</code></pre>
-        </div>
-    )
+  return (
+    <div className="center-children">
+      <pre className="format-sql">
+        <code className="language-SQL" dangerouslySetInnerHTML={{ __html: highlighted }} />
+      </pre>
+    </div>
+  )
 }
